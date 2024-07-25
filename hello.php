62f935a9ca38425886f6a150758669bef5580694
ibraloyza <ibraahim3523@gmail.com>
2024-07-26 02:09:46 +0300

    commit php  variables

--- hello.php
+++ hello.php
@@ -1,7 +1,15 @@
 <?php
 $txt = "php";
-echo "i have $txt";
-echo "hello world ";
-
+echo "i have $txt<br>";
+echo "hello world <br> ";
+$id = 44;
+$name = "ibraahim hassan ahmed";
+$age = 24;
+$phone = "+252618363523";
+$email = "ibraahim3523@gmail.com";
+echo "$id<br>";
+echo "$name<br>";
+echo "$phone<br>";
+echo "$email<br>";
 
 ?>
